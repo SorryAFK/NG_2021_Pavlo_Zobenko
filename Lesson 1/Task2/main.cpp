a79357047c98c03d889414bd68046fae92c2b6b9
SorryAFK <pa2003sha@gmail.com>
2021-07-09 22:27:43 +0300

    edit task 2

--- Lesson 1/Task2/main.cpp
+++ Lesson 1/Task2/main.cpp
@@ -6,11 +6,9 @@ int main()
 {
     int num1;
     int num2;
-    int sum;
     cout << "Vedite chislo 1:  "<< endl;
     cin >> num1;
     cout << "Vedite chislo 2:  " << endl;
     cin >> num2;
-    sum = num1 + num2;
-    cout << "Summa " << sum << endl;
+    cout << "Summa " << num1 + num2 << endl;
 }
