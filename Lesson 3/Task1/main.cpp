2c75b2a3eec2799bdafb3008c415938359bc4939
SorryAFK <pa2003sha@gmail.com>
2021-07-28 18:25:11 +0300

    finished work with style and name of variable

--- Lesson 3/Task1/main.cpp
+++ Lesson 3/Task1/main.cpp
@@ -5,21 +5,22 @@ using namespace std;
 int main()
 {
     int arr [10];
-    int sch;
-    int a;
+    int school;
+    int chekschool;
+    int i;
     cout << "Enter 10 school numbers" << endl;
-    for (int i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++) {
         cout << "[" << i + 1 << "]" << ": ";
         cin >> arr[i];
     }
     cout << "Enter your school number ";
-    cin >> sch;
-    for(int i  =0; i < 10; i++){
-        if (sch == arr[i]) {
-            a = 1;
+    cin >> school;
+    for(i =0; i < 10; i++){
+        if (school == arr[i]) {
+            chekschool = 1;
         }
     }
-    if (a == 1){
+    if (chekschool == 1){
         cout << "i know this school!" << endl;
     }
     else {
