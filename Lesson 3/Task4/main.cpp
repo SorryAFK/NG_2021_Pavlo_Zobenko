bd70767794e6bc19b313dbb0969b9405c6b10a7d
SorryAFK <pa2003sha@gmail.com>
2021-08-12 21:14:52 +0300

    Finished work with Task4, update error when user write 20 numbers of 2 and 1 and software give only 2 strings

--- Lesson 3/Task4/main.cpp
+++ Lesson 3/Task4/main.cpp
@@ -4,28 +4,36 @@ using namespace std;
 
 int main()
 {
-    int num[20];
+    int num[40];
     int maximum = 0;
     cout << "Values: ";
     for (int i = 0; i < 20; i++) {
         cout << "[" << i + 1 << "]" << ": ";
         cin >> num[i];
-        if(num[i] == 0){
+        if(num[i] <= 0) {
             break;
         }
-        if(maximum < num[i]){
+        if(maximum < num[i]) {
             maximum = num[i];
         }
-
     }
-    for(int i = 0; i < maximum; i++){
-        for(int enter = 0; enter < (maximum - (num[i] + enter)); enter++){
+    for(int i = 0; i < 20; i++) {
+        for(int enter = 0; enter <= (maximum  - (num[i] + enter)); enter++) {
             cout << " ";
+            if(num[i] > 100) {
+                break;
+            }
         }
-        for(int enter = 0; enter != num[i]; enter++){
-        cout << "*";
-    }
+        for(int enter = 0; enter != num[i]; enter++) {
+            if(num[i] > 100) {
+                break;
+            }
+            cout << "*";
+        }
+        if(num[i] == 0) {
+            break;
+        }
         cout << endl;
+        continue;
     }
-return 0;
 }
