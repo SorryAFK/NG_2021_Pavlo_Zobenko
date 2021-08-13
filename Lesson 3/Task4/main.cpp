1a37d15453a1738e5a966ff20222e4faf92c8904
SorryAFK <pa2003sha@gmail.com>
2021-08-13 17:54:49 +0300

    Deleted unused if

--- Lesson 3/Task4/main.cpp
+++ Lesson 3/Task4/main.cpp
@@ -7,31 +7,23 @@ int main()
     int num[40];
     int maximum = 0;
     cout << "Values: ";
-    for (int i = 0; i < 20; i++) {
-        cout << "[" << i + 1 << "]" << ": ";
-        cin >> num[i];
-        if(num[i] <= 0) {
+    int iteratacii = 0;
+    for (iteratacii = 0; iteratacii < 20; iteratacii++) {
+        cout << "[" << iteratacii + 1 << "]" << ": ";
+        cin >> num[iteratacii];
+        if(num[iteratacii] <= 0) {
             break;
         }
-        if(maximum < num[i]) {
-            maximum = num[i];
+        if(maximum < num[iteratacii]) {
+            maximum = num[iteratacii];
         }
     }
-    for(int i = 0; i < 20; i++) {
+    for(int i = 0; i < iteratacii; i++) {
         for(int enter = 0; enter <= (maximum  - (num[i] + enter)); enter++) {
             cout << " ";
-            if(num[i] > 100) {
-                break;
-            }
         }
         for(int enter = 0; enter != num[i]; enter++) {
-            if(num[i] > 100) {
-                break;
-            }
             cout << "*";
-        }
-        if(num[i] == 0) {
-            break;
         }
         cout << endl;
         continue;
