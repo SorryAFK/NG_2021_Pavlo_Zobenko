f3d7615b75e48869a5581f1dd2e1e5e4ddad534f
SorryAFK <pa2003sha@gmail.com>
2021-08-14 17:42:31 +0300

    Added new folder Lesson 4

---
+++ Lesson 4/Task1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char text[50];
+    int slov = 0;
+    int simvol = 0;
+    int i = 0;
+    cin.getline(text,50);
+    while(text[i] != 0){
+        if(simvol == 0){
+            if(text[i] >= 'a' && text[i] <= 'z'){
+                simvol = 1;
+                slov++;
+            }
+        }
+        else if(text[i] >= ' ' && text[i] <= '@'){
+            simvol = 0;
+        }
+        i++;
+    }
+    cout << slov;
+}
+
